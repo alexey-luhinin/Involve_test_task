c2eb9fd3876708b9e316fbfec3b7a4d9ef360cbc
Alexey <podzakon11@gmail.com>
2021-08-06 22:28:40 +0300

    fetch

--- static/main.js
+++ static/main.js
@@ -1,13 +1,22 @@
-// let response = fetch('/', {
-//     method: 'POST',
-//     body: `{"amount": "10.0", "description": "hello"}`
-// });
-
-// console.log(response)
-
 document.addEventListener('DOMContentLoaded', function(){
     let btn = document.querySelector('input[type=submit]');
     btn.addEventListener('click', function(event){
         event.preventDefault();
-    })
-})
+        let amount = document.querySelector('input[name=amount]').value;
+        let currency = document.querySelector('select[name=currency]').value;
+        let shop_id = document.querySelector('input[name=shop_id]').value;
+        let shop_order_id = document.querySelector('input[name=shop_order_id]').value;
+
+        fetch('/', {
+            method: 'POST',
+            body: `{"amount": "${amount}", "currency": "${currency}", "shop_id": "${shop_id}", "shop_order_id": "${shop_order_id}"}`
+        }).then(function(response) {
+            response.json().then(function(value) {
+                document.querySelector('input[name=sign]').value = value.sign
+                document.forms['pay'].submit()
+            });
+        });
+        
+
+    });
+});
