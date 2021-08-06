67e9ff72523f962782ba72779e187ab5c77bfa50
Alexey <podzakon11@gmail.com>
2021-08-06 22:27:43 +0300

    Added Enum currency

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
 '''Configurations'''
+from enum import Enum
 
 SECRET = 'SecretKey01'
+
+
+class Currency(Enum):
+    EUR = 978
+    USD = 840
+    RUB = 643
+
+
+if __name__ == '__main__':
+    print(Currency.EUR.value)
