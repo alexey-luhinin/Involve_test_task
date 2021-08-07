c8cb8f133fa85a9ecc02f0ac4c81ab8530b91fe5
Alexey <podzakon11@gmail.com>
2021-08-07 13:46:42 +0300

    Added Payment dataclass

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 '''Configurations'''
 from enum import Enum
+from dataclasses import dataclass
+from typing import List
 
 SECRET = 'SecretKey01'
 
@@ -8,6 +10,12 @@ class Currency(Enum):
     EUR = 978
     USD = 840
     RUB = 643
+
+
+@dataclass
+class Payment:
+    uri: str
+    required_fields: List[str]
 
 
 if __name__ == '__main__':
