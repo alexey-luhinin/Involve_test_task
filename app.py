fa96ff2d6cf10aca23811e354a04827ebd155e11
Alexey <podzakon11@gmail.com>
2021-08-07 13:47:55 +0300

    Created logic for EUR

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
-from config import SECRET
+import json
+from flask import Flask, render_template, request, redirect, jsonify
+from forms import PayForm
+from config import SECRET, Payment, Currency
 from utils import list_to_str, get_sha256
 
 app = Flask(__name__)
@@ -7,19 +9,34 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    form = PayForm()
     if request.method == 'POST':
-        user_request = dict(request.form)
-        keys_sorted = sorted(user_request)
-        values = [user_request.get(key) for key in keys_sorted]
-        sign = list_to_str(values) + SECRET
-        sign_hashed = get_sha256(sign)
-        full_requests = user_request.copy()
-        full_requests['sign'] = sign_hashed
-        # print(sign, sign_hashed)
-        # print(keys_sorted)
-        # print(values)
-        print(full_requests)
-    return render_template('index.html')
+        user_request = request.get_json()
+        currency = user_request['currency']
+
+        if currency == str(Currency.EUR.value):
+            eur_payment = Payment(
+                uri='https://pay.piastrix.com/en/pay',
+                required_fields=['amount', 'currency',
+                                 'shop_id', 'shop_order_id']
+            )
+
+        # user_request = json.loads(request.data.decode())
+        # currency = user_request['currency']
+
+            keys_sorted = sorted(eur_payment.required_fields)
+            values = [user_request.get(key) for key in keys_sorted]
+            sign = list_to_str(values) + SECRET
+            sign_hashed = get_sha256(sign)
+            print(sign, sign_hashed)
+
+            response = dict(zip(keys_sorted, values))
+            response['sign'] = sign_hashed
+            response['uri'] = eur_payment.uri
+
+            return jsonify(response)
+
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
