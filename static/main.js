4fa4251da6e2b29503fb6c79bc1f91f90478dbc4
Alexey <podzakon11@gmail.com>
2021-08-07 13:20:47 +0300

    fetch and

--- static/main.js
+++ static/main.js
@@ -2,21 +2,39 @@ document.addEventListener('DOMContentLoaded', function(){
     let btn = document.querySelector('input[type=submit]');
     btn.addEventListener('click', function(event){
         event.preventDefault();
-        let amount = document.querySelector('input[name=amount]').value;
-        let currency = document.querySelector('select[name=currency]').value;
-        let shop_id = document.querySelector('input[name=shop_id]').value;
-        let shop_order_id = document.querySelector('input[name=shop_order_id]').value;
+        let amount = document.getElementById('amount')
+        let currency = document.getElementById('currency')
+        let description = document.getElementById('description')
+
+        let entry = {
+            amount: amount.value,
+            currency: currency.value,
+            description: description.value,
+            shop_id: '5',
+            shop_order_id: '109'
+        };
 
         fetch('/', {
             method: 'POST',
-            body: `{"amount": "${amount}", "currency": "${currency}", "shop_id": "${shop_id}", "shop_order_id": "${shop_order_id}"}`
+            body: JSON.stringify(entry),
+            headers: new Headers({
+                'content-type': 'application/json'
+            })
         }).then(function(response) {
             response.json().then(function(value) {
-                document.querySelector('input[name=sign]').value = value.sign
-                document.forms['pay'].submit()
+                Object.keys(value).forEach(function(key) {
+                    field = document.getElementById(key)
+                    if (field == null) {
+                        let input = document.createElement('input');
+                        input.setAttribute('type', 'hidden');
+                        input.setAttribute('name', key);
+                        input.setAttribute('value', value[key]);
+                        form = document.getElementById('pay')
+                        form.appendChild(input)
+                        form.submit()
+                    }
+                });
             });
         });
-        
-
     });
 });
