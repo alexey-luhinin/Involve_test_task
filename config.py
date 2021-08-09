fa91fdeca4c56d30d5f84fddb3a456dc40ebe434
Alexey <podzakon11@gmail.com>
2021-08-09 10:21:19 +0300

    got rid useless

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 '''Configurations'''
 from enum import Enum
-from dataclasses import dataclass
-from typing import List
 
 SECRET = 'SecretKey01'
 
@@ -10,12 +8,6 @@ class Currency(Enum):
     EUR = 978
     USD = 840
     RUB = 643
-
-
-@dataclass
-class Payment:
-    uri: str
-    required_fields: List[str]
 
 
 if __name__ == '__main__':
