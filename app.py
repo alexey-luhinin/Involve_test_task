2a19218ab392251d605649bfaabffaf7053d5167
Alexey <podzakon11@gmail.com>
2021-08-09 10:20:42 +0300

    EUR & USD methods works

--- app.py
+++ app.py
@@ -1,55 +1,80 @@
 '''Views'''
 from typing import List
-from flask import Flask, render_template, request, jsonify
+from flask import (
+    Flask,
+    render_template,
+    request,
+    jsonify,
+    redirect,
+    url_for,
+    session
+)
 import requests
 from config import SECRET, Payment, Currency
 from forms import PayForm
 from utils import list_to_str, get_sha256
 
 app = Flask(__name__)
+app.secret_key = 'test'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = PayForm()
+    form = PayForm(request.form)
     if request.method == 'POST':
         user_request = request.get_json()
+        amount = user_request['amount']
         currency = user_request['currency']
 
         if currency == str(Currency.EUR.value):
-            eur_payment = Payment(
-                uri='https://pay.piastrix.com/en/pay',
-                required_fields=['amount', 'currency',
-                                 'shop_id', 'shop_order_id']
-            )
 
-            data = add_sign(eur_payment.required_fields, user_request)
-            data['url'] = eur_payment.uri
+            required_fields = ['amount', 'currency',
+                               'shop_id', 'shop_order_id']
+
+            shop = {
+                'shop_id': '5',
+                'shop_order_id': '109'
+            }
+
+            full_request = {**user_request, **shop}
+
+            data = add_sign(required_fields, full_request)
             data['method'] = 'POST'
-
+            data['action'] = 'https://pay.piastrix.com/en/pay'
             return jsonify(data)
 
         if currency == str(Currency.USD.value):
-            usd_payment = Payment(
-                uri='https://core.piastrix.com/bill/create',
-                required_fields=['shop_amount', 'shop_currency',
-                                 'shop_id', 'shop_order_id',
-                                 'payer_currency']
-            )
 
-            data = add_sign(usd_payment.required_fields, user_request)
+            required_fields = ['shop_amount', 'shop_currency',
+                               'shop_id', 'shop_order_id',
+                               'payer_currency']
+
+            shop = {
+                'shop_id': '5',
+                'shop_order_id': '109',
+                'shop_amount': amount,
+                'shop_currency': '840',
+                'payer_currency': currency,
+                'payer_account': 'support@piastrix.com'
+            }
+
+            full_request = {**shop}
+
+            data = add_sign(required_fields, full_request)
 
             response = requests.post(
-                url=usd_payment.uri,
+                url='https://core.piastrix.com/bill/create',
                 json=data,
             )
 
-            data_json = response.json()['data']
+            data_json = response.json().get('data')
             data_json['method'] = 'GET'
+            data_json['action'] = data_json['url']
+            del data_json['url']
             return jsonify(data_json)
 
-        if currency == str(Currency.RUB.value):
-            pass
+        # if currency == str(Currency.RUB.value):
+        #     pass
 
     return render_template('index.html', form=form)
 
@@ -57,6 +82,7 @@ def index():
 def add_sign(required_fields: List[str], user_request: dict) -> dict:
     '''Returns dictionary result which contains user request data + sign'''
     keys_sorted = sorted(required_fields)
+    print(keys_sorted)
     values = [user_request.get(key) for key in keys_sorted]
     sign = list_to_str(values) + SECRET
     sign_hashed = get_sha256(sign)
