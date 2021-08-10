14daafad64cc47ac000c1592dee71ad60301d3bf
Alexey <podzakon11@gmail.com>
2021-08-10 19:43:36 +0300

    Added logger config

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
 '''Configurations'''
+import os
+from pathlib import Path
 from enum import Enum
+from loguru import logger
+
+log_dir = Path.cwd() / '.log'
+
+if not log_dir.exists():
+    os.mkdir(log_dir)
+
+logger.add(".log/log.log")
 
 SECRET = 'SecretKey01'
 ERROR_MESSAGE = 'Something went wrong...'
 
 
 class Currency(Enum):
+    '''Currency'''
     EUR = 978
     USD = 840
     RUB = 643
